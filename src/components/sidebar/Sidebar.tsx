ed926bf2932ef97dc9b2cdb1abc1c0c38a090151
burakkrt <burak.kurt@glomil.com>
2024-08-15 16:19:14 +0300

    add profile sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import useUserStore from '@/store/useStore'
 import { ISidebarProps } from './types'
 
 function Sidebar({}: ISidebarProps) {
-  return <div className="sidebar">Sidebar</div>
+  const { name, surname, email } = useUserStore()
+
+  return (
+    <div className="sidebar">
+      <div className="profile">
+        <span className="name">{`${name} ${surname}`}</span>
+        <span className="email">{email}</span>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
