eaec3d949ecd00fe39751968d695936a50ea1864
Burak Kurt <krtburak@outlook.com>
2024-08-24 21:20:28 +0300

    fc

--- src/components/metin2-draft-list-card/Metin2DraftListCard.tsx
+++ src/components/metin2-draft-list-card/Metin2DraftListCard.tsx
@@ -3,9 +3,30 @@ import moment from 'moment'
 import { useRouter } from 'next/router'
 import { toast } from 'react-toastify'
 import Button from '@/components/base/button'
-import { IMetin2DraftListCardProps } from './types'
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material'
+import { IDialog, IDialogTypes, IInitialDialog, IMetin2DraftListCardProps } from './types'
+
+const liveActionDialog: IInitialDialog = {
+  title: 'Sunucu yayınlansın mı ?',
+  text: "Bu işlemden sonra sunucu yayına alınacak. Bu işlemi yapmadan önce tüm bilgilerin doğru olduğundan emin ol. Eğer bir şeyler yanlış giderse tekrar sunucuyu taslak 'a çevirebilirsin.",
+}
+
+const draftActionDialog: IInitialDialog = {
+  title: 'Sunucu taslağa alınsın mı ?',
+  text: 'Bu işlemden sonra sunucu yayından kaldırılıp taslak olarak kaydedilecek. Taslak sürecinde düzenleyebilir ve tekrar yayına alabilirsin.',
+}
 
 function Metin2DraftListCard({ data }: IMetin2DraftListCardProps) {
+  const [dialog, setDialog] = React.useState<IDialog>({
+    ...liveActionDialog,
+    isOpen: false,
+  })
   const { id, name, openingDate, status } = data
   const router = useRouter()
 
@@ -17,12 +38,21 @@ function Metin2DraftListCard({ data }: IMetin2DraftListCardProps) {
     }
   }
 
-  const handleLiveServer = () => {
-    alert('yayınla')
+  const handleCloseDialog = () => {
+    setDialog((prev) => ({ ...prev, isOpen: false }))
   }
 
-  const handleDraftServer = () => {
-    alert('taslağa çek')
+  const openDialog = (type: IDialogTypes) => {
+    switch (type) {
+      case 'live':
+        setDialog({ ...liveActionDialog, isOpen: true })
+        break
+      case 'draft':
+        setDialog({ ...draftActionDialog, isOpen: true })
+        break
+      default:
+        break
+    }
   }
 
   return (
@@ -40,7 +70,7 @@ function Metin2DraftListCard({ data }: IMetin2DraftListCardProps) {
             variant="contained"
             size="small"
             color="secondary"
-            onClick={handleDraftServer}
+            onClick={() => openDialog('draft')}
           >
             Taslağa çevir
           </Button>
@@ -51,7 +81,7 @@ function Metin2DraftListCard({ data }: IMetin2DraftListCardProps) {
               variant="contained"
               size="small"
               color="success"
-              onClick={handleLiveServer}
+              onClick={() => openDialog('live')}
             >
               Yayına al
             </Button>
@@ -66,6 +96,36 @@ function Metin2DraftListCard({ data }: IMetin2DraftListCardProps) {
           </>
         )}
       </div>
+      <Dialog
+        open={dialog.isOpen}
+        onClose={handleCloseDialog}
+        aria-labelledby="draft-dialog-title"
+        aria-describedby="draft-dialog-description"
+        className="draft-list-action-dialog"
+      >
+        {dialog?.title && (
+          <DialogTitle className="draft-dialog-title">{dialog.title}</DialogTitle>
+        )}
+
+        <DialogContent>
+          <DialogContentText className="draft-dialog-text">
+            {dialog.text}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} variant="contained" color="error">
+            Vazgeç
+          </Button>
+          <Button
+            onClick={handleCloseDialog}
+            autoFocus
+            variant="contained"
+            color="success"
+          >
+            Tamamla
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
--- src/components/metin2-draft-list-card/types.ts
+++ src/components/metin2-draft-list-card/types.ts
@@ -1,5 +1,15 @@
 import { IServerStatus } from '@/utils/types'
 
+export type IDialogTypes = 'live' | 'draft'
+
+export interface IDialog {
+  isOpen: boolean
+  title?: string
+  text: string
+}
+
+export type IInitialDialog = Omit<IDialog, 'isOpen'>
+
 export interface IMetin2DraftListCardProps {
   data: IServerStatus
 }
